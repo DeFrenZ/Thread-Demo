a71034ac16c8f4796fd0d6726c338e2067530531
Davide De Franceschi <defrenz@me.com>
2019-10-09 10:39:08 +0100

    Initial Commit

---
+++ Thread Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Thread Demo
+//
+//  Created by Davide De Franceschi on 09/10/2019.
+//  Copyright © 2019 Logic Craft Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Thread DemoTests/Thread_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Thread_DemoTests.swift
+//  Thread DemoTests
+//
+//  Created by Davide De Franceschi on 09/10/2019.
+//  Copyright © 2019 Logic Craft Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import Thread_Demo
+
+class Thread_DemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
