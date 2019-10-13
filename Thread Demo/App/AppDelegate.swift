691cb2e55f01019d6dc8665394fa6dfb4ee4e7bc
Davide De Franceschi <defrenz@me.com>
2019-10-13 19:01:00 +0100

    Remove unnecessary delegate method
    As the method docs specify, if you have the details set up in the Info.plist, the default implementation of the method already does the same thing

--- Thread Demo/App/AppDelegate.swift
+++ Thread Demo/App/AppDelegate.swift
@@ -3,15 +3,4 @@ import UIKit
 @UIApplicationMain
 final class AppDelegate: UIResponder {}
 
-extension AppDelegate: UIApplicationDelegate {
-	func application(
-		_ application: UIApplication,
-		configurationForConnecting connectingSceneSession: UISceneSession,
-		options: UIScene.ConnectionOptions
-	) -> UISceneConfiguration {
-		return UISceneConfiguration(
-			name: "Default Configuration",
-			sessionRole: connectingSceneSession.role
-		)
-	}
-}
+extension AppDelegate: UIApplicationDelegate {}
