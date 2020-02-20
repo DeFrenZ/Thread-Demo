d617ae90a688eebfe0bb87a011d9c90430d717e6
Davide De Franceschi <defrenz@me.com>
2020-02-20 11:21:11 +0000

    Move `FetchError` integration to separate file
    This will potentially interest multiple stores integrations

--- Thread Demo/API to Core/DataStore+API.swift
+++ Thread Demo/API to Core/DataStore+API.swift
@@ -38,30 +38,3 @@ private extension API.Client {
 			.eraseToAnyPublisher()
 	}
 }
-
-extension FetchError {
-	init(anyError error: Error) {
-		switch error {
-		case let apiError as API.Client.APIError:
-			self.init(apiError: apiError)
-		default:
-			self = .badResource
-		}
-	}
-
-	init(apiError: API.Client.APIError) {
-		switch apiError {
-		case .invalidURL,
-			 .errorResponse(400 ..< 500):
-			self = .incorrectFetch
-		case .errorResponse(500 ..< 600),
-			 .decoding,
-			 .unrecognized:
-			self = .badResource
-		case .network,
-			 .notAnHTTPResponse,
-			 .errorResponse:
-			self = .remoteUnavailable
-		}
-	}
-}
