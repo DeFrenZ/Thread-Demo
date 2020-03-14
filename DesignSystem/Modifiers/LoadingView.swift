0ccf3a64b2f657d425b2f1d9ee48c90b747b0c3b
Davide De Franceschi <defrenz@me.com>
2020-03-14 19:37:10 +0000

    Animate loader with fade

--- DesignSystem/Modifiers/LoadingView.swift
+++ DesignSystem/Modifiers/LoadingView.swift
@@ -16,31 +16,53 @@ public struct LoadingView<Content: View>: View {
     public var body: some View {
         content
 			.accessibility(hidden: showLoader)
-			.overlay(showLoader ? loader : nil)
+			.overlay(loader)
     }
 
 	private var loader: some View {
 		ZStack {
+			// This should animate by moving the blur radius instead, but it's not publicly available so unless fiddling with animation states — as animating an effect change does animate the blur radius — or using `CIFilter`s, it's not possible.
 			BlurEffect(style: .regular)
-				.edgesIgnoringSafeArea([.bottom, .leading, .trailing])
+				.edgesIgnoringSafeArea(.all)
 			ActivityIndicator(style: .large)
 		}
+			// It should be possible to do this effect by using `AnyTransition.opacity` instead, but I didn't manage to make it work with adding/removing views from a `ZStack`
+			.opacity(showLoader ? 1 : 0)
+			.animation(.default)
 	}
 }
 
 struct LoadingView_Previews: PreviewProvider {
-    static var previews: some View {
-		LoadingView(showLoader: true) {
-			Text("""
-				Some example text,
-				which happens to be quite long,
-				for having a bit of blur effect to go through along
-				with all that white
-				"""
-			)
-				.fontWeight(.black)
-				.expand()
+	private struct Preview: View {
+		@State private var showLoading: Bool = true
+		var body: some View {
+			LoadingView(showLoader: showLoading) {
+				Text("""
+					Some example text,
+					which happens to be quite long,
+					for having a bit of blur effect to go through along
+					with all that white
+					"""
+				)
+					.fontWeight(.black)
+					.expand()
+			}
+				.overlay(
+					Button(action: { self.showLoading.toggle() }) {
+						Text("Toggle loading")
+							.padding()
+							.background(Color.blue)
+							.cornerRadius(15)
+							.foregroundColor(.white)
+					}
+						.expand()
+						.offset(x: 0, y: 100)
+				)
 		}
+	}
+
+    static var previews: some View {
+		Preview()
 			.previewLayout(.device)
     }
 }
