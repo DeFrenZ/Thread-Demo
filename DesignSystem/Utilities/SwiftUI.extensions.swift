a48620677c1e44cb6a6922817e19b1c6a7b142af
Davide De Franceschi <defrenz@me.com>
2020-03-14 19:37:19 +0000

    Simplify utility

--- DesignSystem/Utilities/SwiftUI.extensions.swift
+++ DesignSystem/Utilities/SwiftUI.extensions.swift
@@ -2,6 +2,6 @@ import SwiftUI
 
 extension View {
 	public func expand() -> some View {
-		frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+		frame(maxWidth: .infinity, maxHeight: .infinity)
 	}
 }
